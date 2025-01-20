c7a5590fac690574caf87588fd43a15da4f9d1bd
kareem2134 <kareemhajyahya2003gmail.com>
2025-01-21 01:39:14 +0200

    handled the error logics in the connections class

--- server/src/main/java/bgu/spl/net/srv/Connections.java
+++ server/src/main/java/bgu/spl/net/srv/Connections.java
@@ -4,9 +4,9 @@ import java.io.IOException;
 
 public interface Connections<T> {
 
-    boolean send(int connectionId, T msg);
+    String send(int connectionId, T msg);
 
-    void send(String channel, T msg);
+    String send(String channel, T msg);
 
     void disconnect(int connectionId);
 }
