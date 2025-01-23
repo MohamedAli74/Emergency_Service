71b715b7292f420c232292366bd5d57cde076408
MohamedAli74 <mohamedali243.123@gmail.com>
2025-01-23 02:33:23 +0200

    comparing between strings is done with equals not with == so i changed that

--- server/src/main/java/bgu/spl/net/api/StompMessagingProtocolImpl.java
+++ server/src/main/java/bgu/spl/net/api/StompMessagingProtocolImpl.java
@@ -1,7 +1,6 @@
 package bgu.spl.net.api;
 
 import bgu.spl.net.impl.stomp.StompFrame;
-import bgu.spl.net.srv.BlockingConnectionHandler;
 import bgu.spl.net.srv.ConnectionHandler;
 import bgu.spl.net.srv.Connections;
 import bgu.spl.net.srv.Connectionsimpl;
@@ -22,22 +21,25 @@ public class StompMessagingProtocolImpl implements StompMessagingProtocol<StompF
     }
 
     public void process(StompFrame message){
-        if(message.getStompCommand()=="DISCONNECT"){
-            shouldTerminate=true;
-            connections.disconnect(ownersConnectionID);//disconnect should be String
-        if(message.getHeaders()[0][1]!="receipt"){
-            sendError(message, "the receipt header is missing/misswritten", message.getHeaders()[0][1]);
-        }else{
-            sendReceipt(message.getHeaders()[0][1]);
+
+
+
+        if(message.getStompCommand().equals("DISCONNECT")){
+            if(!message.getHeaders()[0][0].equals("receipt")){
+                sendError(message, "the receipt header is missing/misswritten", message.getHeaders()[0][1]);
+            }else{
+                shouldTerminate=true;
+                sendReceipt(message.getHeaders()[0][1]);
+                connections.disconnect(ownersConnectionID);
         
         }
     }
-        if(message.getStompCommand() == "UNSUBSCRIBE"){
+        if(message.getStompCommand().equals("UNSUBSCRIBE")){
             String receipt=null;
             int id=-1;
             for(String[] header : message.getHeaders()){
-                if(header[0] == "receipt")receipt = header[1];
-                if(header[0] == "id")id = Integer.parseInt(header[1]);  
+                if(header[0].equals("receipt"))receipt = header[1];
+                if(header[0].equals("id"))id = Integer.parseInt(header[1]);  
             }
             String response = connections.unSubscribe(ownersConnectionID,id);
             if(!response.equals("true")){
@@ -45,13 +47,15 @@ public class StompMessagingProtocolImpl implements StompMessagingProtocol<StompF
             }
             if(receipt!=null && response.equals("true"))sendReceipt(receipt);
         }
-        if(message.getStompCommand() == "SUBSCRIBE"){
+        if(message.getStompCommand().equals("SUBSCRIBE")){
+
+
             String destination = null , receipt = null;
             int id=-1;
             for(String[] header : message.getHeaders()){
-                if(header[0] == "receipt")receipt = header[1];
-                if(header[0] == "id")id = Integer.parseInt(message.getHeaders()[0][1]);
-                if(header[0] == "destination")destination = header[1];  
+                if(header[0] .equals("receipt"))receipt = header[1];
+                if(header[0] .equals("id"))id = Integer.parseInt(header[1]);
+                if(header[0] .equals("destination"))destination = header[1];  
             }
             String response =connections.subscribe(ownersConnectionID,destination,id);
             if(!response.equals("true")){
@@ -61,22 +65,24 @@ public class StompMessagingProtocolImpl implements StompMessagingProtocol<StompF
             if(receipt!=null && response.equals("true"))sendReceipt(receipt);
         }
     }
-        if(message.getStompCommand() == "SEND"){
+        if(message.getStompCommand().equals("SEND")){
             String destination=null;
             String receipt=null;
             for(String[] header : message.getHeaders()){
-                if(header[0] == "receipt")receipt = header[1];
-                if(header[0] == "destination")destination = header[1];
+                if(header[0] .equals("receipt"))receipt = header[1];
+                if(header[0] .equals("destination"))destination = header[1];
             }
             
             String[][] headers = {{"subscription",ownersConnectionID.toString()},{"message-id" , connections.generateMessageID().toString()},{"destination" , destination}};
             StompFrame msg = new StompFrame("MESSAGE", headers, message.getFrameBody());
-            String response = connections.send(destination,msg);
+            String response = connections.send(destination,msg,ownersConnectionID);
+            
             if(!response.equals("true")){
                 sendError(message, response, receipt);
-            }if(receipt!=null && response.equals("true"))sendReceipt(receipt);
+            }
+            if(receipt!=null && response.equals("true"))sendReceipt(receipt);
         }
-        if(message.getStompCommand() == "CONNECT"){
+        if(message.getStompCommand().equals("CONNECT")){
             String userName = null ;
             String passCode = null ;
             String host = null ;
@@ -84,30 +90,32 @@ public class StompMessagingProtocolImpl implements StompMessagingProtocol<StompF
             String receipt = null;
 
             for(String[] header : message.getHeaders()){
-                if(header[0] == "accept-version")acceptVersion = header[1];
-                if(header[0] == "host")host = header[1];
-                if(header[0] == "passcode")passCode = header[1];
-                if(header[0] == "login")userName = header[1];
-                if(header[0] == "receipt")receipt = header[1];
+                if(header[0] .equals("accept-version"))acceptVersion = header[1];
+                if(header[0] .equals("host"))host = header[1];
+                if(header[0] .equals("passcode"))passCode = header[1];
+                if(header[0] .equals("login"))userName = header[1];
+                if(header[0] .equals("receipt"))receipt = header[1];
             }
             String response  = connections.connect(acceptVersion,host,userName,passCode,connectionHandler);
-            if(!(receipt.substring(0, 4)).equals("true")){
-                start(Integer.parseInt(response.substring(4, response.length())),connections);
+            if(!(response.substring(0, 4)).equals("true")){
                 sendError(message, response, receipt);
             }else{
+                start(Integer.parseInt(response.substring(4, response.length())),connections);
+                StompFrame connected = new StompFrame("CONNECTED", new String[][]{{"version" , acceptVersion}} , null);
+
+                connections.send(ownersConnectionID, connected);
                 if(receipt!=null)sendReceipt(receipt);
-            }
-            
-            
+            }    
         }
     }
 
     private void sendReceipt(String receiptId){
         String[][] h = new String[1][2];
-            h[0][0] = "receipt-id";
-            h[0][1]=receiptId; 
-            StompFrame receipt = new StompFrame("RECEIPT",h, "");
-            connections.send( ownersConnectionID,receipt);
+        h[0][0] ="receipt-id";
+        h[0][1]=receiptId; 
+        StompFrame receipt = new StompFrame("RECEIPT",h, "");
+        connections.send( ownersConnectionID , receipt);
+
     }
     
     private void sendError(StompFrame FaultyFrame,String message,String receiptId){
@@ -120,7 +128,13 @@ public class StompMessagingProtocolImpl implements StompMessagingProtocol<StompF
         StompFrame error = new StompFrame(
                     "ERROR" ,
                      headers ,
-                     "The message:\n----\n"+FaultyFrame.toString()+"\n----");
+                     "The message:\n----\n"+FaultyFrame.toString()+"\n----"
+        );
+        
+        
+        //in the case of logging out then trying to login with false info
+        //(thus needing to send an error) we can't send the error to the disconnected user 
+
         connections.send(ownersConnectionID, error);
     }
 
