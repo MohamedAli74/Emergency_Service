add194907d32ae5e295582fef6482f10e9795ad4
MohamedAli74 <mohamedali243.123@gmail.com>
2025-01-28 21:05:42 +0200

    Summary: a class that represents a summary call output

---
+++ client/include/summary.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <vector>
+#include "Report.h"
+#include "event.h"
+
+
+class Summary{
+private:
+
+    std::string channel_name;
+    int Total;
+    int active;
+    int forces_arrival_at_scene;
+    std::vector<Report> Events_Report;
+
+public:
+    
+    Summary(std::string channel_name, int Total, int active, int forces_arrival_at_scene, std::vector<Report> Events_Report);
+    Summary();
+    ~Summary();
+    const std::string &get_channel_name() const;
+    int get_Total() const;
+    int get_active() const;
+    int get_forces_arrival_at_scene() const;
+    const std::vector<Report> &get_Events_Report() const;
+    
+};
+
+Summary createSummary(const std::vector<Event>& event_strings, const std::string& channel_name);
+
---
+++ client/src/summary.cpp
@@ -0,0 +1,48 @@
+#include "summary.h"
+
+
+    const std::string& Summary::get_channel_name() const{
+        return channel_name;
+    }
+    int Summary::get_Total() const{
+        return Total;
+    }
+    int Summary::get_active() const{
+        return active;
+    }
+    int Summary::get_forces_arrival_at_scene() const{
+        return forces_arrival_at_scene;
+    }
+    const std::vector<Report>& Summary::get_Events_Report() const{
+        return Events_Report;
+    }
+    
+    Summary createSummary(const std::vector<Event>& event_strings, const std::string& channel_name){
+        Summary ouput;
+        int activeCounter = 0;
+        int forcesCounter = 0;
+        std::vector<Report> Events_Report;
+
+        for (int i = 0; i < event_strings.size(); i++){
+            Event event = event_strings.at(i);
+            if(event.get_general_information().at("active") == "true"){
+                activeCounter++;
+            }
+            if(event.get_general_information().at("forces arrival at scene") == "true"){
+                forcesCounter++;
+            }
+            Report report(event.get_city(), epochToDateTime(event.get_date_time()), event.get_name(), event.get_description());
+            Events_Report.push_back(report);
+        }
+        ouput = Summary(channel_name, event_strings.size(), activeCounter, forcesCounter, Events_Report);
+        return ouput;
+    }
+
+    	std::string epochToDateTime(int epoch) {
+		time_t time = epoch;
+		struct tm *timeinfo;
+		timeinfo = localtime(&time);
+		char buffer[80];
+		strftime(buffer, 80, "%d/%m/%y_%H:%M", timeinfo);
+		return buffer;
+	}
